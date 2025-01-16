f1af62c7d06645ab7731d49c542c7c4074a8c741
Ethan55674 <ekohlas0231@floridapoly.edu>
2025-01-16 16:29:36 +0000

    hello

---
+++ test_file/Student_info_test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+// create student ID and course ID
+int student_id = 2271;
+int id = 10234;
+int dif_id = (id - student_id);
+// create student name
+
+printf("Name is John Doe \n course id is %d\n student id is %d\n and the difference between ids is %d\n ", id, student_id, dif_id);
+
+
+}
